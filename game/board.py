f365985bfb2ed07d6afd4a66110655c81277dbab
Eric Wilson <wilson.eric.n@gmail.com>
2014-06-03 22:29:53 -0400

    sliding with merging

--- game/board.py
+++ game/board.py
@@ -29,19 +29,35 @@ class Board(object):
     def _flip_vert(self):
         self.tiles = {(3-x,y):self.tiles[(x,y)] for (x,y) in self.tiles}
 
-    def _slide(self,axis):
+    def _find_groups(self, axis):
         primary = axis[0]
         secondary = axis[1]
         keys = self.tiles.keys()
         keys = sorted(keys, key=lambda x: x[primary])
         groups = []
-        for k, group in groupby(keys,key=lambda x: x[primary]):
+        for k, group in groupby(keys, key=lambda x: x[primary]):
             groups.append(sorted(list(group), key=lambda x: x[secondary]))
+        return groups, primary
 
-        for row in groups:
-            for idx, key in enumerate(row):
-                newkey = (key[primary],idx) if primary == 0 else (idx,key[primary])
-                if newkey != key:
-                    self.tiles[newkey] = self.tiles[key]
+    def _slide(self,axis):
+        groups, primary = self._find_groups(axis)
+
+        for group in groups:
+            previous_val = -1
+            previous_key = None
+            merges = 0
+            for idx, key in enumerate(group):
+                print previous_val
+                if self.tiles[key] == previous_val:
+                    self.tiles[previous_key] = 2*previous_val
                     del self.tiles[key]
-
+                    merges += 1
+                    previous_val = -1
+                else:
+                    pos = idx - merges
+                    newkey = (key[primary],pos) if primary == 0 else (pos,key[primary])
+                    if newkey != key:
+                        self.tiles[newkey] = self.tiles[key]
+                        del self.tiles[key]
+                    previous_key = newkey
+                    previous_val = self.tiles[newkey]
--- game/board_test.py
+++ game/board_test.py
@@ -43,3 +43,28 @@ def test_board_slide_down_without_merge():
     b.slide_down()
 
     assert b.tiles == {(2,1):2,(3,3):4,(3,1):3,(3,2):8}
+
+def test_slide_left_with_merge():
+    b = Board()
+    b.tiles[(0,1)]= 4
+    b.tiles[(0,2)]= 4
+    b.tiles[(0,3)]= 4
+
+    b.slide_left()
+
+    assert b.tiles == {(0,0):8,(0,1):4}
+
+
+def test_slide_up_with_merge():
+    b = Board()
+    b.tiles = {(0,0):4,(0,1):2,(0,2):2,
+        (1,0):4,(1,1):8,(1,2):2,(1,3):8,
+        (2,0):4,(2,1):8,(2,2):4,(2,3):8,
+        (3,0):4,(3,1):8,(3,3):2}
+
+    b.slide_up()
+
+    assert b.tiles == {(0,0):8,(0,1):2,(0,2):4,(0,3):16,
+                       (1,0):8,(1,1):16,(1,2):4,(1,3):2,
+                       (2,1):8}
+
